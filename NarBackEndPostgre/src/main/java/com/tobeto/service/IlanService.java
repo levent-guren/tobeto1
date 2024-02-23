ac187952ddf8ed89144e00023478b6b06ee14f8b
Levent GUREN <levent.guren@gmail.com>
2024-02-23 20:37:01 +0300

    guncelleme

---
+++ NarBackEndPostgre/src/main/java/com/tobeto/service/IlanService.java
@@ -0,0 +1,17 @@
+package com.tobeto.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tobeto.entity.YazilimIlan;
+import com.tobeto.repository.YazilimIlanRepository;
+
+@Service
+public class IlanService {
+	@Autowired
+	private YazilimIlanRepository yazilimIlanRepository;
+
+	public YazilimIlan yazilimIlanVer(YazilimIlan yazilimIlan) {
+		return yazilimIlanRepository.save(yazilimIlan);
+	}
+}
