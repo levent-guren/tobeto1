bda91ee31c941edb84e9c0a75469a23d71d28002
Levent GUREN <levent.guren@gmail.com>
2024-02-23 19:58:25 +0300

    guncelleme

--- NarBackEnd/src/main/java/com/tobeto/config/SecurityConfig.java
+++ NarBackEnd/src/main/java/com/tobeto/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tobeto.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.tobeto.filter.JwtAuthorizationFilter;
 
 @Configuration
 public class SecurityConfig {
+	@Autowired
+	private JwtAuthorizationFilter jwtAuthorizationFilter;
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,8 +28,7 @@ public class SecurityConfig {
 				.anyRequest().authenticated()
 				)
 		     .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		     //.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-		     ;
+		     .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 		// @formatter:on
 
 		return http.build();
